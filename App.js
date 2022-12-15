492c7cb1445f9e90e7843db3f5fa3581dfce9ca1
Sneezan <ceciliafraanlund@gmail.com>
2022-12-15 23:38:17 +0100

    added styled components

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
+      <Text>gripp app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const Text = styled.Text`
+color: red;
+font-size: 40px;
+`
 
 const styles = StyleSheet.create({
   container: {

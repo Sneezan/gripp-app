c83590d3d8cb5d1f5980ee870b49bea91c15d2f0
Sneezan <ceciliafraanlund@gmail.com>
2023-01-02 12:18:07 +0100

    added swipe function to cards and gamepage

--- screens/GamePage.js
+++ screens/GamePage.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet } from 'react-native';
+// import styled from 'styled-components/native'
+import Swiper from 'react-native-deck-swiper'
 import styled from 'styled-components/native'
+import { View, StyleSheet } from 'react-native';
 import { API_URL } from "../utils/utils.js";
-//{statements.body} to get statements
-// maybe an event.preventDefault? to stop page from reloading when changing card?
 
 export const GamePage = () => {
   const [statements, setStatement] = useState({})
@@ -22,27 +21,41 @@ export const GamePage = () => {
     fetchStatements()
     },[]);
 
-
   return (
-    <View style={styles.container} >
-
-      <Card> 
-      <Text>{statements.body}</Text>  
-      </Card>
-
-      <StatusBar style="light" />
+<View style={styles.container}>
+        <Swiper
+            cards={[`${statements.body}`, `${statements.body}`, `${statements.body}`]}  // need to loop this?
+            renderCard={(card) => {
+                return (
+                  <View style={styles.wrap}>
+                    <CardStyle style={styles.card}>
+                        <Text style={styles.text}>{card}</Text>
+                    </CardStyle>
+                    </View>
+                )
+            }}
+            onSwiped={(cardIndex) => {console.log(cardIndex)}}
+            onSwipedAll={() => {console.log('onSwipedAll')}}
+            cardIndex={0}
+            backgroundColor={'#2d2d2d'}
+            stackSize= {3}>
+        </Swiper>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2d2d2d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#2d2d2d',
+
+    },
+    wrap: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+}); 
 
 const Text = styled.Text`
 color: red;
@@ -51,7 +64,7 @@ font-size: 30px;
 text-transform: uppercase;
 `
 
-const Card = styled.View`
+const CardStyle = styled.View`
 width: 330px;
 height: 480px;
 background-color: white ;

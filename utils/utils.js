4a96618e6d90dcb3b3e2efc602f4ace170d35b5a
Sneezan <ceciliafraanlund@gmail.com>
2023-01-10 12:47:53 +0100

    changes

--- utils/utils.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = "https://grip-backend-eom6wfm7xa-uc.a.run.app/";
-
-export const API_URL = (slug) => `${BASE_URL}/${slug}`;
-
-//  https://grip-backend-eom6wfm7xa-uc.a.run.app/  backend URL 
